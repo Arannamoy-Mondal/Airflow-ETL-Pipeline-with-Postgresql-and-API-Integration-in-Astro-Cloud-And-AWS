56a20f336f3ed9a60bd0cede1d4814aad22141ac
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-03-25 08:44:25 +0600

    upd

---
+++ Readme.md
@@ -0,0 +1,6 @@
+#### Workflow
+```mermaid
+A1[API] --> A2[Transformation]
+A2[Transformation] --> A3 [JSON]
+A3[JSON] --> A4[Database]
+```
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from airfloe-etl-pipeline-with-postgresql-and-api-integration-in-astro-cloud-and-aws!")
+
+
+if __name__ == "__main__":
+    main()
